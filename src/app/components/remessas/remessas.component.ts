c3d1d330d4336b352742e0169b6b1d2cc80758c5
Pedro Santana Mendes <pmendes09@hotmail.com>
2022-08-23 04:56:33 -0300

    o final de um pesadelo

--- src/app/components/remessas/remessas.component.ts
+++ src/app/components/remessas/remessas.component.ts
@@ -1,88 +1,88 @@
-// import { FileHandle } from './../../_model/file-handle.model';
-// import { Component, OnInit } from '@angular/core';
-// import { DomSanitizer } from '@angular/platform-browser';
-// // import { Image } from 'src/app/_model/image.model';
-// import { NgForm } from '@angular/forms';
-// import { RemessasService } from 'src/app/remessas/services/remessas.service';
-// import { HttpErrorResponse } from '@angular/common/http';
-// import { Router } from '@angular/router';
-// import { MatSnackBar } from '@angular/material/snack-bar';
+import { FileHandle } from './../../_model/file-handle.model';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+// import { Image } from 'src/app/_model/image.model';
+import { NgForm } from '@angular/forms';
+import { RemessasService } from 'src/app/remessas/services/remessas.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-// @Component({
-//   selector: 'app-remessas',
-//   templateUrl: './remessas.component.html',
-//   styleUrls: ['./remessas.component.css'],
-// })
-// export class RemessasComponent implements OnInit {
-//   image: Image = {
-//     productImages: [],
-//   };
+@Component({
+  selector: 'app-remessas',
+  templateUrl: './remessas.component.html',
+  styleUrls: ['./remessas.component.css'],
+})
+export class RemessasComponent implements OnInit {
+  // image: Image = {
+  //   productImages: [],
+  // };
 
-//   constructor(
-//     private remessa: RemessasService,
-//     private sanitizer: DomSanitizer,
-//     private router: Router,
-//     private snackBar: MatSnackBar
-//   ) {}
+  constructor(
+    private remessa: RemessasService,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
-//   ngOnInit() {}
+  ngOnInit() {}
 
-//   addImage(imageForm: NgForm) {
-//     const remessaFormData = this.prepareFormData(this.image);
+  addImage(imageForm: NgForm) {
+    // const remessaFormData = this.prepareFormData(this.image);
 
-//     // this.remessa.save(remessaFormData).subscribe(
-//     //   (response: Image) => {
-//     //     this.goToImagesList();
-//     //   },
-//     //   (error: HttpErrorResponse) => {
-//     //     console.log(error);
-//     //   }
-//     // );
-//   }
+    // this.remessa.save(remessaFormData).subscribe(
+    //   (response: Image) => {
+    //     this.goToImagesList();
+    //   },
+    //   (error: HttpErrorResponse) => {
+    //     console.log(error);
+    //   }
+    // );
+  }
 
-//   goToImagesList() {this.router.navigate(['/remessa']);}
+  goToImagesList() {this.router.navigate(['/remessa']);}
 
-//   prepareFormData(remessa: Image): FormData {
-//     const formData = new FormData();
+  // prepareFormData(remessa: Image): FormData {
+  //   const formData = new FormData();
 
-//     for (var i = 0; i < remessa.productImages.length; i++) {
-//       formData.append(
-//         'file',
-//         remessa.productImages[i].file,
-//         remessa.productImages[i].file.name
-//       );
-//     }
-//     return formData;
-//   }
+  //   for (var i = 0; i < remessa.productImages.length; i++) {
+  //     formData.append(
+  //       'file',
+  //       remessa.productImages[i].file,
+  //       remessa.productImages[i].file.name
+  //     );
+  //   }
+  //   return formData;
+  // }
 
-//   url = '.assets';
+  url = '.assets';
 
-//   onselectFile(event: any) {
-//     if (event.target.files) {
-//       const file = event.target.files[0];
+  onselectFile(event: any) {
+    if (event.target.files) {
+      const file = event.target.files[0];
 
-//       this.remessa.UploadImage(file)
-//       .subscribe(data=>{this.onSucces()}, error => {this.onError()}
+      this.remessa.UploadImage(file)
+      .subscribe(data=>{this.onSucces()}, error => {this.onError()}
 
-//       )
+      )
 
-//       const fileHandle: FileHandle = {
-//         file: file,
-//         url: this.sanitizer.bypassSecurityTrustUrl(
-//           window.URL.createObjectURL(file)
-//         ),
-//       };
+      const fileHandle: FileHandle = {
+        file: file,
+        url: this.sanitizer.bypassSecurityTrustUrl(
+          window.URL.createObjectURL(file)
+        ),
+      };
 
-//       this.image.productImages.push(fileHandle);
-//     }
-//   }
+      // this.image.productImages.push(fileHandle);
+    }
+  }
 
-//   private onSucces(){
-//     this.snackBar.open('Imagem salva com sucesso','',{duration:5000});
-//   }
+  private onSucces(){
+    this.snackBar.open('Imagem salva com sucesso','',{duration:5000});
+  }
 
-//   private onError(){
-//     this.snackBar.open('Erro ao salvar Imagem','',{duration:5000});
-//   }
+  private onError(){
+    this.snackBar.open('Erro ao salvar Imagem','',{duration:5000});
+  }
 
-// }
+}
